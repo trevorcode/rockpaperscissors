42eec43cf990e0f00c8f02520afb6d87e31aa9d1
Trevor <trevorcode@outlook.com>
2023-07-04 22:59:13 -0600

    Add rock paper scissors game logic

--- src/rockpaperscissors/core.clj
+++ src/rockpaperscissors/core.clj
@@ -12,14 +12,15 @@
                :rounds []
                :current-round 1})
 
-(defn determine-winner [{{p1Action :action} :player1
-                         {p2Action :action} :player2}]
+(defn determine-round-winner
+  "Determines the round winner of the rock paper scissors"
+  [p1Action p2Action]
   (cond
     (= p1Action p2Action)
     :tie
 
     (or (and (= p1Action :rock)
-             (= p2Action :paper))
+             (= p2Action :scissors))
         (and (= p1Action :paper)
              (= p2Action :rock))
         (and (= p1Action :scissors)
@@ -28,9 +29,37 @@
 
     :else :player2))
 
-(defn calc-winner? [state]
+(defn both-actions-chosen?
+  "Are both rock paper scissors actions chosen?"
+  [state]
   (and (some? (-> state :player1 :action))
        (some? (-> state :player2 :action))))
+
+(defn check-for-match-winner
+  "Receives a list of rounds and a first-to number of wins
+   and determines if there is a match winner"
+  [rounds first-to]
+  (let [p1-wins (count (filter #(= (:winner %) :player1) rounds))
+        p2-wins (count (filter #(= (:winner %) :player2) rounds))]
+    (cond
+      (>= p1-wins first-to) :player1
+      (>= p2-wins first-to) :player2
+      :else nil)))
+
+(defn conclude-round [state]
+  (let [player1-action (-> state :player1 :action)
+        player2-action (-> state :player2 :action)
+
+        old-round {:round (state :current-round)
+                   :player1-action player1-action
+                   :player2-action player2-action
+                   :winner (determine-round-winner player1-action player2-action)}]
+    (-> state
+        (assoc-in [:player1 :action] nil)
+        (assoc-in [:player2 :action] nil)
+        (update :current-round inc)
+        (update :rounds conj old-round)
+        (as-> $ (assoc $ :winner (check-for-match-winner (:rounds $) 3))))))
 
 (defn player-action [state {action :action player-name :player}]
   (let [player (if (=  player-name (-> state :player1 :name))
@@ -63,8 +92,9 @@
             (= action :paper)
             (= action :scissors))
         (let [new-state (player-action state event)]
-          (if (calc-winner? new-state)
-            (recur new-state)
+          (if (and (both-actions-chosen? new-state)
+                   (nil? (state :winner)))
+            (recur (conclude-round new-state))
             (recur new-state)))
 
         :else (do
@@ -82,10 +112,11 @@
     (swap! games-channel-map assoc game-id game-ref)
     game-id))
 
-(comment (start-new-game! "Tim" "Tammy"))
+(comment (start-new-game! "Tom" "Tony"))
 (comment (println @games-channel-map))
 (comment (go (>! (-> @games-channel-map first val :channel) :game-state)))
-(comment (go (>! (-> @games-channel-map first val :channel) {:action :scissors :player "Tim"})))
+(comment (go (>! (-> @games-channel-map first val :channel) {:action :rock :player "Tom"})))
+(comment (go (>! (-> @games-channel-map first val :channel) {:action :paper :player "Tony"})))
 (comment (close! (-> @games-channel-map first val :channel)))
 
 (defn -main
